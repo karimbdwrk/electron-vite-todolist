d3c87623fc02713112d47cdfab650bfdb486c7c3
Karim Badwork <karim@badwork.fr>
2023-11-21 09:42:51 +0100

    tsx to jsx

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import AuthProvider from "./auth/AuthProvider";
+
+import HomePage from "./pages/HomePage";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+import ProfilePage from "./pages/ProfilePage";
+import EditProfilePage from "./pages/EditProfilePage";
+import ChangePassword from "./pages/ChangePassword";
+
+import Header from "./components/Header";
+import ToDoList2 from "./components/ToDoList2";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<AuthProvider>
+				<Router>
+					<Header />
+					<Routes>
+						<Route path='/' element={<HomePage />} />
+						<Route path='/signin' element={<SignInPage />} />
+						<Route path='/signup' element={<SignUpPage />} />
+						<Route path='/profile' element={<ProfilePage />} />
+						<Route
+							path='/editprofile'
+							element={<EditProfilePage />}
+						/>
+						<Route
+							path='/changepassword'
+							element={<ChangePassword />}
+						/>
+						<Route path='/todolist/:id' element={<ToDoList2 />} />
+					</Routes>
+				</Router>
+			</AuthProvider>
+		</>
+	);
+}
+
+export default App;
